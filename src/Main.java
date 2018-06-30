3900f37627aa6c6fc93f30d2f57d851b528624d8
ANNOHD <nick2004.ngt@gmail.com>
2018-06-30 22:41:55 +0200

    test1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+
 public class Main {
 
+public static void main(String args[]){
+    JDA jda = new JDABuilder(AccountType.BOT).setToken("token").buildBlocking();
 
+
+}
 
 }
